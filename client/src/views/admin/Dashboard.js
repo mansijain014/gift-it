7558a2669e79bf11b1f1be41d6f466614f4f2601
Mansi <mansi142002@gmail.com>
2021-06-12 03:44:59 +0530

    Update

--- client/src/views/admin/Dashboard.js
+++ client/src/views/admin/Dashboard.js
@@ -1,17 +1,13 @@
 import React from "react";
-import Navbar from "components/Navbars/AuthNavbar.js";
-// components
+import {useState} from 'react'
+// import Navbar from "components/Navbars/AuthNavbar.js";
 
-import CardLineChart from "components/Cards/CardLineChart.js";
-import CardBarChart from "components/Cards/CardBarChart.js";
-import CardPageVisits from "components/Cards/CardPageVisits.js";
-import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+function Dashboard() {
 
-export default function Dashboard() {
   return (
     <>
 <section class="p-6 dark:bg-coolGray-800 dark:text-coolGray-50 mt-24 pt-20">
-	<form novalidate="" action="" class="container flex flex-col mx-auto space-y-12 add-form ng-untouched ng-pristine ng-valid">
+	<form class="container flex flex-col mx-auto space-y-12 add-form ng-untouched ng-pristine ng-valid">
 		<fieldset class="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-coolGray-900">
 			<div class="space-y-2 col-span-full lg:col-span-1">
 				<p class="font-medium font-semibold pb-140">Add Item Info</p>
@@ -26,7 +22,7 @@ export default function Dashboard() {
 			<div class="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
 				<div class="col-span-full">
           <p>Item Description</p>
-					<textarea id="bio" placeholder="Describe your gift item" class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-coolGray-700 dark:text-coolGray-900"></textarea>
+					<textarea id="bio" placeholder="Describe your gift item" class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-coolGray-700 dark:text-coolGray-900" ></textarea>
 				</div>
         <div class="col-span-full">
           <p>Add Item Image</p>
@@ -43,3 +39,12 @@ export default function Dashboard() {
     </>
   );
 }
+
+
+export default Dashboard
+
+
+
+
+
+
