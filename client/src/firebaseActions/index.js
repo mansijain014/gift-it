fb95b43a1cbaeac83ed26078b4cc2bd62c17c74f
Nafees Nehar <nafees87n@gmail.com>
2021-06-12 18:46:22 +0530

    added signin

--- client/src/firebaseActions/index.js
+++ client/src/firebaseActions/index.js
@@ -1,16 +1,27 @@
 import { firebaseAuth, db } from "../firebase";
-import { signUp } from "../redux/actions";
+import { signIn, signUp } from "../redux/actions";
 
 export const userSignUp = async (data, dispatch) => {
   const { email, password, name } = data;
   const error = await firebaseAuth
     .createUserWithEmailAndPassword(email, password)
     .then((res) => {
-      dispatch(signUp({ email, name, uid: res.user.uid }));
-      db.collection("users").doc(res.user.uid).set({ email, name });
+      const userData = { email, name, uid: res.user.uid };
+      dispatch(signUp(userData));
+      db.collection("users").doc(res.user.uid).set(userData);
       return null;
     })
     .catch((err) => err.message);
 
   return error;
+};
+
+export const userSignIn = async (data, dispatch) => {
+  const { email, password } = data;
+  firebaseAuth.signInWithEmailAndPassword(email, password).then((res) => {
+    db.collection("users")
+      .doc(res.user.uid)
+      .get()
+      .then((doc) => dispatch(signIn(doc.data())));
+  });
 };
