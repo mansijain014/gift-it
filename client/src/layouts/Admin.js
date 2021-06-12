dca698940c6a1cb112444708f17f0738f02ec6d4
Nafees Nehar <nafees87n@gmail.com>
2021-06-13 02:29:05 +0530

    fix: margin

--- client/src/layouts/Admin.js
+++ client/src/layouts/Admin.js
@@ -25,7 +25,7 @@ export default function Admin() {
         <AdminNavbar />
         {/* Header */}
         <HeaderStats />
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+        <div className="px-4 md:px-10 mx-auto w-full">
           <Switch>
             <Route path="/admin/mygiftlist" exact component={MyGiftList} />
             <Route path="/admin/profile" exact component={Profile} />
--- client/src/views/admin/Exchange.js
+++ client/src/views/admin/Exchange.js
@@ -18,10 +18,10 @@ export default function Exchange() {
   }, []);
 
   return (
-    <div className="flex flex-wrap">
+    <div className="flex flex-wrap my-10 py-10">
       {items.map((item) => {
         return (
-          <div className="w-full md:w-4/12 px-4 mr-auto ml-auto mt-32 pt-20">
+          <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
               <img
                 alt="..."
