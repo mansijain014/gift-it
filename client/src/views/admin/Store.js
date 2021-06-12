f3168e53a6431904e3ac4e15bfae8fba847d2ef1
Mansi <mansi142002@gmail.com>
2021-06-12 20:15:48 +0530

    Updated file

--- client/src/views/admin/Store.js
+++ client/src/views/admin/Store.js
@@ -1,32 +1,32 @@
-import React from 'react'
+// import React from 'react'
 
 
-//function clickMe(){
-    //alert('you will be redirected');
-//}
-const Store = () => {
-    return (
-       <div className="mt-28 pt-20">
+// //function clickMe(){
+//     //alert('you will be redirected');
+// //}
+// const Store = () => {
+//     return (
+//        <div className="mt-28 pt-20">
        
-            <a href="https://www.flipkart.com/" target="_blank">
-               <button> Click Me-Flipkart </button>
-            </a>
-            <a href= "https://www.amazon.in/" target="_blank">
-                <button>  Click Me-Amazon</button>
-            </a>  
-            <a href= "https://www.myntra.com/" target="_blank">
-                <button>  Click Me-Myntra</button>
-            </a>  
-            <a href= "https://www.snapdeal.com/" target="_blank">
-                <button>  Click Me-Snapdeal</button>
-            </a>   
-            <a href= "https://www.giftcart.com/" target="_blank">
-                <button>  Click Me-Giftcart</button>
-            </a>  
+//             <a href="https://www.flipkart.com/" target="_blank">
+//                <button> Click Me-Flipkart </button>
+//             </a>
+//             <a href= "https://www.amazon.in/" target="_blank">
+//                 <button>  Click Me-Amazon</button>
+//             </a>  
+//             <a href= "https://www.myntra.com/" target="_blank">
+//                 <button>  Click Me-Myntra</button>
+//             </a>  
+//             <a href= "https://www.snapdeal.com/" target="_blank">
+//                 <button>  Click Me-Snapdeal</button>
+//             </a>   
+//             <a href= "https://www.giftcart.com/" target="_blank">
+//                 <button>  Click Me-Giftcart</button>
+//             </a>  
 
         
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default Store
+// export default Store
